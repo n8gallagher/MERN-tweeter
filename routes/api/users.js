23a0125de45160da58378cdc25628c774d5ab385
nateg <n8gallagher@gmail.com>
2020-08-29 16:11:39 -0700

    added jwt to users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const keys = require('../../config/keys')
+const jwt = require('jsonwebtoken');
 
 router.get("/test", (req, res) => {
   res.json({ msg: "This is the users route" });
@@ -48,7 +50,22 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            res.json({msg: 'Success'});
+            const payLoad = {
+              id: user.id,
+              handle: user.handle,
+              email: user.email
+            }
+            jwt.sign(
+              payLoad, 
+              keys.secretOrKey,
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: "Bearer" + token
+                })
+              }
+              )
           } else {
             return res.status(400).json({password: 'Incorrect password'});
           }
