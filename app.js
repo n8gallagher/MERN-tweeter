146bd60834914e997877d109c481079b0ac638b9
nateg <n8gallagher@gmail.com>
2020-08-29 15:57:36 -0700

    set up routes for register, added bcrypt for storing password digest

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require("express");
-const mongoose = require('mongoose');
 const app = express();
+const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const User = require('./models/User');
+const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-app.get("/", (req, res) => res.send("Get your tweet on"));
 
-const port = process.env.PORT || 3001;
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  const user = new User ({
+    handle: "myHandle",
+    email: "name@hotmail.com",
+    password: "hunter12"
+  })
+  user.save();
+  res.send("Get your tweet on");
+});
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
