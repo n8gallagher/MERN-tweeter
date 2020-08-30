56403790241a7cbe9484333071da9d3a561136f6
nateg <n8gallagher@gmail.com>
2020-08-29 21:40:58 -0700

    added tweets api

--- routes/api/tweets.js
+++ routes/api/tweets.js
@@ -1,8 +1,31 @@
 const express = require("express");
+const passport = require("passport");
 const router = express.Router();
+const validateTweetInput = require("../../validation/tweets");
+const Tweet = require("../../models/Tweet");
 
 router.get("/test", (req, res) => {
   res.json({ msg: "This is the tweet route" });
-})
+});
+
+router.post("/", 
+  passport.authenticate("jwt", {session: false }),
+  (req, res) => {
+    const { isValid, errors } = validateTweetInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newTweet = new Tweet({
+      user: req.user.id,
+      text: req.body.text
+    });
+
+    newTweet
+      .save()
+      .then(tweet => res.json(tweet))
+  }
+)
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -4,12 +4,19 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const keys = require('../../config/keys')
 const jwt = require('jsonwebtoken');
+const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 router.get("/test", (req, res) => {
   res.json({ msg: "This is the users route" });
 })
 
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  
+  if(!isValid) {
+    return res.status(400).json(errors);
+  }
   // Check to make sure nobody has already registered with a duplicate email
   User.findOne({ email: req.body.email })
     .then(user => {
@@ -38,6 +45,12 @@ router.post('/register', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   const email = req.body.email;
   const password = req.body.password;
 
